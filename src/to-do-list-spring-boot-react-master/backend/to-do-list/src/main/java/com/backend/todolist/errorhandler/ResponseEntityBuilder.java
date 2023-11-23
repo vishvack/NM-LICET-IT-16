5a05d0c4c41b930f9368d0b220a7683374bdc24f
Vishvack <101174178+vishvack@users.noreply.github.com>
2023-11-23 21:12:28 +0530

    Delete src/to-do-list-spring-boot-react-master directory

--- src/to-do-list-spring-boot-react-master/backend/to-do-list/src/main/java/com/backend/todolist/errorhandler/ResponseEntityBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.backend.todolist.errorhandler;
-
-import  org.springframework.http.ResponseEntity;
-
-public class ResponseEntityBuilder {
-    public static ResponseEntity<Object> build(CustomException customException) {
-          return new ResponseEntity<>(customException, customException.getStatus());
-    }
-}
